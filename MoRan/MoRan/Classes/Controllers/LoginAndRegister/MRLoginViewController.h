b9501005c530137962ecb91ba68b3d344f84cd56
yiqingsheng <250071954@qq.com>
2015-09-20 23:27:03 +0800

    建立工程
    创建工程，把项目的目录结构搭建好了

---
+++ MoRan/MoRan/Classes/Controllers/LoginAndRegister/MRLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  MRLoginViewController.h
+//  MoRan
+//
+//  Created by yikobe_mac on 15/9/20.
+//  Copyright © 2015年 yikobe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MRLoginViewController : UIViewController
+
+@end
---
+++ MoRan/MoRan/Classes/Controllers/LoginAndRegister/MRLoginViewController.m
@@ -0,0 +1,27 @@
+//
+//  MRLoginViewController.m
+//  MoRan
+//
+//  Created by yikobe_mac on 15/9/20.
+//  Copyright © 2015年 yikobe. All rights reserved.
+//
+
+#import "MRLoginViewController.h"
+
+@interface MRLoginViewController ()
+
+@end
+
+@implementation MRLoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+
+@end
