d10faff1fac0ae7aa02172a711a6f9ac06e1d550
yiqingsheng <250071954@qq.com>
2015-10-20 12:39:59 +0800

    commit

--- GBMoran_iOS/Controllers/Square/Publish/GBMPublishViewController.h
+++ GBMoran_iOS/Controllers/Square/Publish/GBMPublishViewController.h
@@ -3,7 +3,7 @@
 //  GBMoran_iOS
 //
 //  Created by yikobe_mac on 15/9/21.
-//  Copyright © 2015年 com.GeekBand. All rights reserved.
+//  Copyright © 2015年 Brave. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
